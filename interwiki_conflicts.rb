46c7488b446f74b770fcaf8136c8887c600ad2e7
MatmaRex <matma.rex@gmail.com>
2011-09-01 22:44:27 +0200

    moved logic out of busy_loop

--- interwiki_conflicts.rb
+++ interwiki_conflicts.rb
@@ -151,6 +151,165 @@ class InterwikiConflictSolver
 		end
 	end
 	
+	def process_command line
+		case line
+		when ''
+			# pass
+			
+		when 'help'
+			puts 'commands:'
+			puts [
+				'help',
+				'show',
+				'showg',
+				'summary <text>',
+				'linksto <lang> <title>',
+				'linksfrom <lang> <title>',
+				'start <lang> <title>',
+				'start <lang>',
+				'rename <group> <group>',
+				'move <lang> <regex> <group>',
+				'move * <regex> <group>',
+				'gather <lang> <title>',
+				'commit',
+				'exit',
+			]
+			
+		when 'exit'
+			break
+			
+		when 'show'
+			@all.each do |pair|
+				puts "#{pair.join(':').encode('cp852', undef: :replace).ljust 40, '.'}#{@groups[pair].encode('cp852', undef: :replace)}"
+			end
+			
+		when 'showg'
+			lists_per_group = {} # {groupname => [pairs]}
+			@groups.each_pair do |pair, group|
+				lists_per_group[group] ||= []
+				lists_per_group[group] << pair
+			end
+			lists_per_group.each_value{|gr| gr.sort!}
+			
+			lists_per_group.each_pair do |group, pairs|
+				puts ">>> #{group}"
+				puts pretty_iw pairs
+				puts ''
+			end
+			
+		when /\Asummary (.+)\Z/
+			@summary = [@summary_base, $1].join ' - '
+			@sf.each_with_index do |kv, i|
+				wiki, s = *kv
+				s.summary = @summary
+			end
+			
+		when /\Alinksfrom ([a-z-]+) (.+)\Z/
+			wiki, title = $1, $2
+			puts pretty_iw @linksfrom[ [wiki, title] ]
+			
+		when /\Alinksto ([a-z-]+) (.+)\Z/
+			wiki, title = $1, $2
+			puts pretty_iw @linksto[ [wiki, title] ]
+		
+		when /\Astart \*\Z/
+			@all.each{|wiki, title| Launchy.open "http://#{wiki}.wikipedia.org/w/index.php?title=#{CGI.escape title}" }
+			
+		when /\Astart ([a-z-]+) (.+)\Z/, /\Astart ([a-z-]+)\Z/
+			wiki, title = $1, $2
+			pairs = title ? [wiki, title] : @all.select{|pair| pair[0]==wiki}
+			
+			pairs.each{|wiki, title| Launchy.open "http://#{wiki}.wikipedia.org/w/index.php?title=#{CGI.escape title}" }
+			
+		when /\Amove \* (.+?) ([a-z0-9]+)\Z/, /\Amove \* ()([a-z0-9]+)\Z/
+			name_reg, target_group = /#{$1}/i, $2
+			
+			got = @all.select{|pair| name_reg=~pair[1]}
+			got.each do |pair|
+				@groups[pair] = target_group
+			end
+		
+		when /\Amove ([a-z-]+) (.+?) ([a-z0-9]+)\Z/, /\Amove ([a-z-]+) ()([a-z0-9]+)\Z/
+			wiki, name_reg, target_group = $1, /#{$2}/i, $3
+			
+			got = @all.select{|pair| pair[0]==wiki and name_reg=~pair[1]}
+			if got.length==1
+				pair = got[0]
+				@groups[pair] = target_group
+			elsif got.empty?
+				puts 'uh oh. no matches!'
+			else
+				puts 'uh oh. multiple matches!'
+			end
+		
+		when /\Agather ([a-z-]+) (.+)\Z/
+			wiki, title = $1, $2
+			gather_from wiki, title
+			
+		when /\Arename ([a-z0-9]+) ([a-z0-9]+)\Z/, /\Amerge ([a-z0-9]+) ([a-z0-9]+)\Z/
+			from, to = $1, $2
+			@groups.each_key do |k|
+				@groups[k] = to if @groups[k]==from
+			end
+			
+		when /\Afind ([a-z\-,]+|\*) (.+)\Z/,  /\Afind ([a-z\-,]+|\*)\Z/
+			wiki, selector = $1, $2
+			puts pretty_iw find_all_matching(wiki, selector)
+			
+		when 'commit'
+			if @logged_in
+				puts "are you sure you know what you're doing? (yes/no)"
+				if gets.strip.downcase=='yes'
+					lists_per_group = {} # {groupname => [pairs]}
+					@groups.each_pair do |pair, group|
+						lists_per_group[group] ||= []
+						lists_per_group[group] << pair
+					end
+					lists_per_group.each_value{|gr| gr.sort!}
+					
+					# pp lists_per_group
+					# gets
+					
+					clear_iw_regex = /\[\[(?:#{@all.map{|a| a[0]}.uniq.join '|'}):.+?\]\](?:\r?\n)?/
+					# p clear_iw_regex
+					# gets
+					
+					lists_per_group.each_pair do |group, pairs|
+						next if group=='donttouch' or group=~/\Areached from/
+						puts group+'...'
+						
+						pairs.each do |pair|
+							wiki, title = *pair
+							page = Page.new title, wiki
+							
+							if page.text.scan(clear_iw_regex).sort == pretty_iw(pairs-[pair]).sort
+								puts "#{(pretty_iw [pair])[0]} - no changes needed"
+							else
+								page.text = (page.text.gsub clear_iw_regex, '').strip
+								page.text += "\n\n" + pretty_iw(pairs-[pair]).join("\n") unless group=='clear'
+								
+								res = page.save
+								puts "#{(pretty_iw [pair])[0]} #{res['edit']['result']=="Success" ? 'saved' : 'failure!!!'}"
+								
+								gets
+							end
+						end
+					end
+					
+					puts 'done!'
+					
+				else
+					puts 'aborted.'
+				end
+			else
+				puts 'not logged in!'
+			end
+			
+		else
+			puts "d'oh? incorrect command."
+		end
+	end
+	
 	def busy_loop
 		puts "#{@all.length} articles in #{@sf.length} languages."
 		puts ''
@@ -170,163 +329,7 @@ class InterwikiConflictSolver
 		while true
 			print '> '
 			read = gets.strip
-			
-			case read
-			when ''
-				# pass
-				
-			when 'help'
-				puts 'commands:'
-				puts [
-					'help',
-					'show',
-					'showg',
-					'summary <text>',
-					'linksto <lang> <title>',
-					'linksfrom <lang> <title>',
-					'start <lang> <title>',
-					'start <lang>',
-					'rename <group> <group>',
-					'move <lang> <regex> <group>',
-					'move * <regex> <group>',
-					'gather <lang> <title>',
-					'commit',
-					'exit',
-				]
-				
-			when 'exit'
-				break
-				
-			when 'show'
-				@all.each do |pair|
-					puts "#{pair.join(':').encode('cp852', undef: :replace).ljust 40, '.'}#{@groups[pair].encode('cp852', undef: :replace)}"
-				end
-				
-			when 'showg'
-				lists_per_group = {} # {groupname => [pairs]}
-				@groups.each_pair do |pair, group|
-					lists_per_group[group] ||= []
-					lists_per_group[group] << pair
-				end
-				lists_per_group.each_value{|gr| gr.sort!}
-				
-				lists_per_group.each_pair do |group, pairs|
-					puts ">>> #{group}"
-					puts pretty_iw pairs
-					puts ''
-				end
-				
-			when /\Asummary (.+)\Z/
-				@summary = [@summary_base, $1].join ' - '
-				@sf.each_with_index do |kv, i|
-					wiki, s = *kv
-					s.summary = @summary
-				end
-				
-			when /\Alinksfrom ([a-z-]+) (.+)\Z/
-				wiki, title = $1, $2
-				puts pretty_iw @linksfrom[ [wiki, title] ]
-				
-			when /\Alinksto ([a-z-]+) (.+)\Z/
-				wiki, title = $1, $2
-				puts pretty_iw @linksto[ [wiki, title] ]
-			
-			when /\Astart \*\Z/
-				@all.each{|wiki, title| Launchy.open "http://#{wiki}.wikipedia.org/w/index.php?title=#{CGI.escape title}" }
-				
-			when /\Astart ([a-z-]+) (.+)\Z/, /\Astart ([a-z-]+)\Z/
-				wiki, title = $1, $2
-				pairs = title ? [wiki, title] : @all.select{|pair| pair[0]==wiki}
-				
-				pairs.each{|wiki, title| Launchy.open "http://#{wiki}.wikipedia.org/w/index.php?title=#{CGI.escape title}" }
-				
-			when /\Amove \* (.+?) ([a-z0-9]+)\Z/, /\Amove \* ()([a-z0-9]+)\Z/
-				name_reg, target_group = /#{$1}/i, $2
-				
-				got = @all.select{|pair| name_reg=~pair[1]}
-				got.each do |pair|
-					@groups[pair] = target_group
-				end
-			
-			when /\Amove ([a-z-]+) (.+?) ([a-z0-9]+)\Z/, /\Amove ([a-z-]+) ()([a-z0-9]+)\Z/
-				wiki, name_reg, target_group = $1, /#{$2}/i, $3
-				
-				got = @all.select{|pair| pair[0]==wiki and name_reg=~pair[1]}
-				if got.length==1
-					pair = got[0]
-					@groups[pair] = target_group
-				elsif got.empty?
-					puts 'uh oh. no matches!'
-				else
-					puts 'uh oh. multiple matches!'
-				end
-			
-			when /\Agather ([a-z-]+) (.+)\Z/
-				wiki, title = $1, $2
-				gather_from wiki, title
-				
-			when /\Arename ([a-z0-9]+) ([a-z0-9]+)\Z/, /\Amerge ([a-z0-9]+) ([a-z0-9]+)\Z/
-				from, to = $1, $2
-				@groups.each_key do |k|
-					@groups[k] = to if @groups[k]==from
-				end
-				
-			when /\Afind ([a-z\-,]+|\*) (.+)\Z/,  /\Afind ([a-z\-,]+|\*)\Z/
-				wiki, selector = $1, $2
-				puts pretty_iw find_all_matching(wiki, selector)
-				
-			when 'commit'
-				if @logged_in
-					puts "are you sure you know what you're doing? (yes/no)"
-					if gets.strip.downcase=='yes'
-						lists_per_group = {} # {groupname => [pairs]}
-						@groups.each_pair do |pair, group|
-							lists_per_group[group] ||= []
-							lists_per_group[group] << pair
-						end
-						lists_per_group.each_value{|gr| gr.sort!}
-						
-						# pp lists_per_group
-						# gets
-						
-						clear_iw_regex = /\[\[(?:#{@all.map{|a| a[0]}.uniq.join '|'}):.+?\]\](?:\r?\n)?/
-						# p clear_iw_regex
-						# gets
-						
-						lists_per_group.each_pair do |group, pairs|
-							next if group=='donttouch' or group=~/\Areached from/
-							puts group+'...'
-							
-							pairs.each do |pair|
-								wiki, title = *pair
-								page = Page.new title, wiki
-								
-								if page.text.scan(clear_iw_regex).sort == pretty_iw(pairs-[pair]).sort
-									puts "#{(pretty_iw [pair])[0]} - no changes needed"
-								else
-									page.text = (page.text.gsub clear_iw_regex, '').strip
-									page.text += "\n\n" + pretty_iw(pairs-[pair]).join("\n") unless group=='clear'
-									
-									res = page.save
-									puts "#{(pretty_iw [pair])[0]} #{res['edit']['result']=="Success" ? 'saved' : 'failure!!!'}"
-									
-									gets
-								end
-							end
-						end
-						
-						puts 'done!'
-						
-					else
-						puts 'aborted.'
-					end
-				else
-					puts 'not logged in!'
-				end
-				
-			else
-				puts "d'oh? incorrect command."
-			end
+			process_command read
 		end
 	end
 end
